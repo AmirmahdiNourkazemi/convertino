24f734cfe3c3a01924e3ccc3207b4186aad0a68b
Amirmahdi Nourkazemi <nourkazemi80@gmail.com>
2024-08-19 16:35:37 +0330

    feat: add logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 /* From Uiverse.io by bhaveshxrawat */ 
 import React from 'react'
 import '../css/footer.css'
+import vadaLogo from "../assets/images/vada.png"
 export default function Footer() {
   return (
     /* From Uiverse.io by bhaveshxrawat */ 
 <a href="https://www.vada.ir/" className='mb-4'><div class="card">
-   <img className='z-50' src="/src/assets/images/vada.png" alt="vada" />
+   <img className='z-50' src={vadaLogo} alt="vada" />
 </div></a>
   )
 }
